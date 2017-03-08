669aff22872a745a35510d837dc809d0e28d58ea
vlad9pa <vlad.milytin@gmail.com>
2017-03-08 16:22:31 +0000

    added mainController

---
+++ src/main/java/com/vlad9pa/tasktest/Controller/MainController.java
@@ -0,0 +1,13 @@
+package com.vlad9pa.tasktest.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+    @RequestMapping(value = {"/index","/"}, method = RequestMethod.GET)
+    private String index(){
+        return "index";
+    }
+}
