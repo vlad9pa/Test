2a5f828eb03f83d3784eb77be1180f31586077f1
vlad9pa <vlad.milytin@gmail.com>
2017-03-08 21:02:24 +0000

    ....

--- src/main/java/com/vlad9pa/tasktest/Repository/PhoneBookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.vlad9pa.tasktest.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PhoneBookRepository extends JpaRepository<PhoneBook, Long>{
-}
