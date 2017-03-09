8ed9d1606648a86bbd819c54eacab8c92b3f635b
vlad9pa <vlad.milytin@gmail.com>
2017-03-09 14:08:58 +0000

    Configured Jpa Data Source

--- src/main/java/com/vlad9pa/tasktest/Application.java
+++ src/main/java/com/vlad9pa/tasktest/Application.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@AutoConfigureDataJpa
 @ComponentScan(basePackages = "com.vlad9pa.tasktest")
 @SpringBootApplication
 public class Application {
